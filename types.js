ed2cdb250bf5ef54cf8198d8e3c8bdd49de4e994
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-16 16:08:45 +0800

    Update and rename types.ts to types.js

---
+++ types.js
@@ -0,0 +1,9 @@
+/**
+ * Types File
+ * * In the original TypeScript version, this file contained interfaces for 
+ * Product, CartItem, and Category, as well as the CategoryId type.
+ * * In JavaScript, these type definitions are removed as they do not 
+ * exist at runtime.
+ */
+
+export {};
